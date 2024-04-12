9625f604f989dfe5b6f857e1f046f3bc470b7a52
jacti <jactio99@gmail.com>
2024-04-13 00:32:42 +0900

    phoneNumber complete

--- hash/src/PhoneNumberBook.java
+++ hash/src/PhoneNumberBook.java
@@ -1,28 +1,22 @@
 import java.util.*;
 public class PhoneNumberBook {
     public boolean solution(String[] phone_book) {
-        
-        boolean answer = re(Arrays.asList(phone_book));
-        return !answer;
+        Arrays.sort(phone_book);
+
+        int index = 0;
+        for(int i=1; i< phone_book.length; i++){
+            if(phone_book[i-1].length()<phone_book[i].length()){
+                for(int j = index ; j < i ; j++){
+                    if(phone_book[i].startsWith(phone_book[j]))return false;
+                }
+            } else{
+                index = i;
+            }
+        }
+        return true;
     }
 
-    //1차시도
-    private boolean re(List<String> phrList){
-        Map<String,List<String>> hashMap = new HashMap<String,List<String>>();
-        for(String phr : phrList){
-            if(phr.length() == 1) return true;
-            String start = phr.substring(0,1);
-            String etc = phr.substring(1);
-            List<String> value = hashMap.getOrDefault(start, new ArrayList<String>());
-            value.add(etc);
-            hashMap.put(start,value);
-        }
-        for(List<String> newPhrList : hashMap.values()){
-            if(newPhrList.size()==1) continue;
-            if(re(newPhrList)) return true;
-        }
-        return false;
-    }
+
 
     public static void main(String[] args) {
         String[] phone_book = {"119", "97674223", "1195524421"};
